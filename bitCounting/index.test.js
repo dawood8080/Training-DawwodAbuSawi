fd10942bc4ee622e02f03962b66e0f5d103e6ce3
dawood8080 <dawood_khader@hotmail.com>
2022-10-25 10:54:12 +0300

    test : adding unit test for bitCounting function

--- bitCounting/index.test.js
+++ bitCounting/index.test.js
@@ -16,3 +16,13 @@ const bitCounting = (num) => {
   }
   return bitCounter;
 };
+
+describe('bitCounting', () => {
+  it('should return the correct number of bits in the binary representation', () => {
+    expect(bitCounting(1234)).toBe(5);
+    expect(bitCounting(8)).toBe(1);
+    expect(bitCounting(17)).toBe(2);
+    expect(bitCounting(1)).toBe(1);
+    expect(bitCounting(2047)).toBe(11);
+  });
+});
