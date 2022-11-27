3676a31fd4bcf45b022fc4b381ade46ff21ece32
dawood8080 <dawood_khader@hotmail.com>
2022-11-27 11:28:34 +0300

    test : unit test for rockPaperScissors

--- rockPaperScissors/index.test.js
+++ rockPaperScissors/index.test.js
@@ -38,3 +38,37 @@ const rockPaperScissors = function(num) {
   getOutcomes(num, []);
   return outcomes;
 };
+
+describe('rockPaperScissors', () => {
+  it('returns sequence of throws a single player in the given rounds', () => {
+    expect(rockPaperScissors(3)).toStrictEqual([
+      [ 'rock', 'rock', 'rock' ],
+      [ 'rock', 'rock', 'paper' ],
+      [ 'rock', 'rock', 'scissors' ],
+      [ 'rock', 'paper', 'rock' ],
+      [ 'rock', 'paper', 'paper' ],
+      [ 'rock', 'paper', 'scissors' ],
+      [ 'rock', 'scissors', 'rock' ],
+      [ 'rock', 'scissors', 'paper' ],
+      [ 'rock', 'scissors', 'scissors' ],
+      [ 'paper', 'rock', 'rock' ],
+      [ 'paper', 'rock', 'paper' ],
+      [ 'paper', 'rock', 'scissors' ],
+      [ 'paper', 'paper', 'rock' ],
+      [ 'paper', 'paper', 'paper' ],
+      [ 'paper', 'paper', 'scissors' ],
+      [ 'paper', 'scissors', 'rock' ],
+      [ 'paper', 'scissors', 'paper' ],
+      [ 'paper', 'scissors', 'scissors' ],
+      [ 'scissors', 'rock', 'rock' ],
+      [ 'scissors', 'rock', 'paper' ],
+      [ 'scissors', 'rock', 'scissors' ],
+      [ 'scissors', 'paper', 'rock' ],
+      [ 'scissors', 'paper', 'paper' ],
+      [ 'scissors', 'paper', 'scissors' ],
+      [ 'scissors', 'scissors', 'rock' ],
+      [ 'scissors', 'scissors', 'paper' ],
+      [ 'scissors', 'scissors', 'scissors' ]
+    ])
+  })
+})
